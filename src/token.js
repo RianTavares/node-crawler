25a6c741a06ed7cab377477a189414bf71151564
Rian Tavares <rian.tavares@live.com>
2018-11-18 21:12:49 -0200

    fixing ESlint warnings of token.js

--- src/token.js
+++ src/token.js
@@ -1,18 +1,16 @@
-const jwt = require("jsonwebtoken");
-const secretKey = "RianNaInfoGlobo";
+/* eslint no-console: ["error", { allow: ["log"] }] */
+const jwt = require('jsonwebtoken');
+
+const secretKey = 'RianNaInfoGlobo';
 module.exports = {
-  createSignature: toBeSigned => {
-    return jwt.sign({ username: toBeSigned }, secretKey, {
-      expiresIn: 60 * 60 // expires in 24 hours
-    });
-  },
-  verifySignature: token => {
-    return jwt.verify(token, secretKey, function(err, decoded) {
-      if (err) {
-        return false;
-      } else {
-        return true;
-      }
-    });
-  }
+  createSignature: toBeSigned => jwt.sign({ username: toBeSigned }, secretKey, {
+    expiresIn: 60 * 60, // expires in 24 hours
+  }),
+  verifySignature: token => jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      console.log(decoded);
+      return false;
+    }
+    return true;
+  }),
 };
