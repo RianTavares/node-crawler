34aa8ac009db0bfa8c7e7f18e89ca400e50f9f1d
Rian Tavares <rian.tavares@live.com>
2018-11-18 20:32:59 -0200

    fixing eslint warnings into app.js

--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
-const express = require("express");
-const routes = require("./routes");
-const bodyParser = require("body-parser");
+/* eslint no-console: ["error", { allow: ["log"] }] */
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+
 const port = process.env.PORT || 3000;
 
 const app = express();
-app.all("*", function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+app.all('*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/api/v1", routes());
+app.use('/api/v1', routes());
 
 app.listen(port, () => console.log(`Server running on PORT: ${port}`));
