5d4b2311dba4183d5fe01f19de84055d5a3a0ab6
Rian Tavares <rian.tavares@live.com>
2018-11-19 22:15:58 -0200

    validating username and password filds

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-/* eslint no-console: ["error", { allow: ["log"] }] */
+/* eslint no-console: ['error', { allow: ['log'] }] */
 const api = require('express').Router();
 const request = require('request-promise-native');
 const cheerio = require('cheerio');
@@ -32,7 +32,42 @@ module.exports = () => {
       if (value.username === username && value.password === password) {
         res.send(jwt.createSignature(username));
         return true;
+      } if (
+        (password !== '' && password !== undefined && username === undefined)
+        || (password !== '' && password !== undefined && username === '')
+      ) {
+        res.send('Oops!\nYou forgot the username, try again!');
+        return false;
+      } if (
+        (username !== '' && username !== undefined && password === undefined)
+        || (username !== '' && username !== undefined && password === '')
+      ) {
+        res.send('Oops!\nYou forgot the password, try again!');
+        return false;
+      } if (
+        username !== undefined
+        && password !== undefined
+        && (value.username !== username && value.password !== password)
+        && (username !== '' && password !== '')
+      ) {
+        res.send('Oops!\nUser not allowed, try again!');
+        return false;
+      } if (
+        (value.username !== username && value.password === password)
+        || (value.username === username && value.password !== password)
+      ) {
+        res.send('Oops!\nUsername or Password incorret!');
+        return false;
+      } if (
+        (username === undefined && password === undefined)
+        || (username === '' && password === '')
+      ) {
+        res.send(
+          'Oops!\nYou forgot the username and password!\nPass both into the body of the request :)',
+        );
+        return false;
       }
+      res.send('Oops something went wrong, please try again.');
       return false;
     });
   });
