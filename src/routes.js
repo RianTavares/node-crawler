157b319b6be30e13b3b2d9c959960475221ad949
Rian Tavares <rian.tavares@live.com>
2018-11-20 00:33:24 -0200

    formating code to ESlint requirements

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 /* eslint no-console: ['error', { allow: ['log'] }] */
+/* eslint prefer-destructuring: ["error", {VariableDeclarator: {object: false}}] */
 const api = require('express').Router();
 const request = require('request-promise-native');
 const cheerio = require('cheerio');
@@ -28,23 +29,26 @@ module.exports = () => {
   api.post('/login', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    const userAllowed = dataBase.filter((value) => {
+    dataBase.filter((value) => {
       if (value.username === username && value.password === password) {
         res.send(jwt.createSignature(username));
         return true;
-      } if (
+      }
+      if (
         (password !== '' && password !== undefined && username === undefined)
         || (password !== '' && password !== undefined && username === '')
       ) {
         res.send('Oops!\nYou forgot the username, try again!');
         return false;
-      } if (
+      }
+      if (
         (username !== '' && username !== undefined && password === undefined)
         || (username !== '' && username !== undefined && password === '')
       ) {
         res.send('Oops!\nYou forgot the password, try again!');
         return false;
-      } if (
+      }
+      if (
         username !== undefined
         && password !== undefined
         && (value.username !== username && value.password !== password)
@@ -52,13 +56,15 @@ module.exports = () => {
       ) {
         res.send('Oops!\nUser not allowed, try again!');
         return false;
-      } if (
+      }
+      if (
         (value.username !== username && value.password === password)
         || (value.username === username && value.password !== password)
       ) {
         res.send('Oops!\nUsername or Password incorret!');
         return false;
-      } if (
+      }
+      if (
         (username === undefined && password === undefined)
         || (username === '' && password === '')
       ) {
